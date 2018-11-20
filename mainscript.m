e93db2177452301ca5a3085439e355a1f1caf0e9
Pearl <pearlayem@gmail.com>
2018-11-20 10:23:06 -0800

    log graph added

--- mainscript.m
+++ mainscript.m
@@ -2,5 +2,24 @@ clear
 close all
 clc
 
-data = xlsread('STRATOGEM_plankton.xls');
-date = datetime(data(:,1),'ConvertFrom','excel');
+[plankton_counts txt raw] = xlsread('STRATOGEM_plankton.xls');
+date = datetime(txt(2:end,1),'ConvertFrom','excel');
+species_names=txt(1,2:end);
+
+row_sums=nansum(plankton_counts,2);
+
+figure
+subplot(211)
+plot(date,row_sums, 'k-', 'Markersize', 15);
+title('Total phytoplankton count per year');
+xlabel('Date');
+ylabel('Pythoplankton Count');
+datetick;
+
+subplot(212)
+plot(date,row_sums, 'k-', 'Markersize', 15);
+title('Total phytoplankton count per year-Logarithmic Scale');
+xlabel('Date');
+ylabel('Pythoplankton Count');
+set(gca, 'yscale', 'log');
+datetick;
